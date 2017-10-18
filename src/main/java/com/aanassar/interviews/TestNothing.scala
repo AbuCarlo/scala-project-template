626c77ff1df1cfe2c16b9c42d0f85f509a866b81
Anthony A. Nassar <anthonyabunassar@gmail.com>
2017-10-18 14:35:55 -0400

    Adding sample unit tests.

---
+++ src/main/java/com/aanassar/interviews/TestNothing.scala
@@ -0,0 +1,33 @@
+package com.aanassar.interviews;
+
+	import org.junit.runner.RunWith
+	import org.scalatest.Finders
+	import org.scalatest.FunSuite
+	import org.scalatest.Matchers
+	import org.scalatest.junit.JUnitRunner
+	import org.scalatest.prop.Checkers
+	import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+	import org.scalatest.prop.GeneratorDrivenPropertyChecks
+	import org.junit.runner.RunWith
+	import org.scalatest.prop.Checkers
+	import org.scalatest.Matchers
+	import org.scalatest.FunSuite
+	import org.scalatest.junit.JUnitRunner
+	import org.scalactic.TolerantNumerics
+	import org.junit.runner.RunWith
+	import org.scalacheck.Gen
+	import org.scalacheck.Prop.propBoolean
+	import org.scalatest.FunSuite
+	import org.scalatest.Inspectors.forEvery
+	import org.scalatest.Matchers
+	import org.scalatest.junit.JUnitRunner
+	import org.scalatest.matchers.HavePropertyMatchResult
+	import org.scalatest.matchers.HavePropertyMatcher
+	import org.scalatest.prop.Checkers
+	import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+	@RunWith(classOf[JUnitRunner])
+	class TestNothing extends FunSuite with Checkers with Matchers {
+
+}
